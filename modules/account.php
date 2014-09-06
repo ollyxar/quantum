be5855cc37312920c5bcc35e632bf3303247258a
Alex <alexslipknot@europe.com>
2014-09-06 07:53:53 +0300

    Updating Account unit - moved methods from library to module

--- modules/account.php
+++ modules/account.php
@@ -11,6 +11,52 @@ class Account extends QModule {
         if ($q) return true; else return false;
     }
 
+    public function register($name, $email, $password) {
+        $result = $this->validate($name, $email, $password);
+        if ($result == '') {
+            $name = $this->engine->db->escape($name);
+            $email = $this->engine->db->escape(strtolower($email));
+            $password = md5(md5($this->engine->db->escape($password)));
+            if ($this->engine->db->query("INSERT INTO " . DB_PREF . "users (`name`, `email`, `password`, `user_group`) VALUES ('" . $name . "', '" . $email . "', '" . $password . "', '5')")) {
+                $this->sendConfirm($email);
+            }
+            return true;
+        } else {
+            return $result;
+        }
+    }
+
+    private function validate($name, $email, $password) {
+        $result = '';
+        $email = $this->engine->db->query(strtolower($email));
+        if (preg_match('/\w+@\w+?\.[a-zA-Z]{2,6}/', $email) && strlen($email) < 40) {
+            $exists = $this->engine->db->query("SELECT COUNT(1) FROM " . DB_PREF . "users WHERE email = '" . $this->engine->db->escape($email) . "'")->row;
+            if (!empty($exists)) {
+                $result .= '1';
+            }
+        } else {
+            $result .= '2';
+        }
+        if (strlen($name) < 2 || strlen($name) > 254) {
+            $result .= '3';
+        }
+        if (strlen($password) < 5 || strlen($password) > 20) {
+            $result .= '4';
+        }
+
+        return (int)$result;
+    }
+
+    private function sendConfirm($email) {
+        $confirm_key = md5(md5($email));
+        $this->engine->db->query("UPDATE " . DB_PREF . "users SET `confirm` = '" . $confirm_key . "' WHERE LOWER(email) = '" . strtolower($email) . "' ");
+        $message = '<h1>Congratulations! You have successfully registered on ' . $_SERVER['HTTP_HOST'] . '</h1>';
+        $message .= '<p>To finish your registration just follow the link.</p>';
+        $message .= '<p><a href="' . $this->engine->url->link('route=account&action=register') . '&amp;confirm_key=' . $confirm_key . '">Confirm registration</a></p>';
+        $message .= '<p>If it was not You just ignore this message.</p>';
+        $this->engine->sendMail($email, 'system@' . $_SERVER['HTTP_HOST'], $_SERVER['HTTP_HOST'] . ' - Notification system', 'Подтверждение регистрации на "Блабла"', $message);
+    }
+
     public function confirm() {
         $this->engine->db->query("UPDATE " . DB_PREF . "users SET `confirm` = '1' WHERE `confirm` = '" . $_GET['confirm_key'] . "'");
     }
