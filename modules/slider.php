df53241dc5ad0559453c90964eba7e76998222db
Olexy <alexslipknot@users.noreply.github.com>
2014-11-28 10:34:15 +0200

    Excluded fixed router (home)

--- modules/slider.php
+++ modules/slider.php
@@ -5,11 +5,9 @@ class Slider extends QModule {
     protected $version = '1.1';
 
     public function index() {
-        if ($_GET['route'] == 'home') {
-            $this->engine->document->addScript(TEMPLATE . 'js/jquery.bxslider.min.js');
-            $this->engine->document->addStyle(TEMPLATE . 'css/jquery.bxslider.css');
-            $this->data['slides']    = $this->params['slides'];
-            $this->template          = TEMPLATE . 'template/widgets/slider.tpl';
-        }
+        $this->engine->document->addScript(TEMPLATE . 'js/jquery.bxslider.min.js');
+        $this->engine->document->addStyle(TEMPLATE . 'css/jquery.bxslider.css');
+        $this->data['slides']    = $this->params['slides'];
+        $this->template          = TEMPLATE . 'template/widgets/slider.tpl';
     }
 }
